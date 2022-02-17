9203bcab7f54ad7b14aa7f80614a2255530aaebf
CharlieCastleWeb <castillomartinezcarlos@gmail.com>
2022-02-17 18:27:28 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+let listItems = "";
+
+inputBtn.addEventListener("click", function() {
+    
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    renderLeads();
+    console.log(myLeads);
+});
+
+function renderLeads() {
+    for(let i = 0; i<myLeads.length; i++) {
+
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems;
+}
