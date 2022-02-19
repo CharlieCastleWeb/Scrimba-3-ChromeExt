e0bec2708a85fca423567edc24d7032a20d850ed
CharlieCastleWeb <castillomartinezcarlos@gmail.com>
2022-02-19 07:44:27 +0100

    Completed

--- index.js
+++ index.js
@@ -3,27 +3,58 @@
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el");
-let listItems = "";
+const tabBtn = document.getElementById("tab-btn");
 
-inputBtn.addEventListener("click", function() {
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    render(myLeads);
+}
+
+
+tabBtn.addEventListener("click", function() {
     
-    myLeads.push(inputEl.value);
-    inputEl.value = "";
-    renderLeads();
-    console.log(myLeads);
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        console.log(tabs);
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    });
+    
 });
 
-function renderLeads() {
-    for(let i = 0; i<myLeads.length; i++) {
-
+function render(leads) {
+    let listItems = "";
+    for(let i = 0; i<leads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `
     }
     ulEl.innerHTML = listItems;
 }
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    // Save the myLeads array to localStorage 
+    // PS: remember JSON.stringify()
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+    
+    // To verify that it works:
+    //console.log( localStorage.getItem("myLeads") )
+})
+
